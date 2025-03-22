084c7e4b1e8eac57cc4a58d97183fcaf0554a5a8
A.A.N. <adrianna.a.nowak@wp.pl>
2025-03-22 03:33:41 +0100

    calculator style update

--- 10 HTML, CSS, JS together/10-exercises-script.js
+++ 10 HTML, CSS, JS together/10-exercises-script.js
@@ -4,8 +4,16 @@
  function toggleButton (buttonName) {
    const button = document.querySelector(`${buttonName}`);
    if (!button.classList.contains('is-toggled')) {
+     turnOffPreviousButton();
      button.classList.add('is-toggled');
    } else {
      button.classList.remove('is-toggled');
    }
  }
+
+ function turnOffPreviousButton() {
+  const previousButton = document.querySelector('.is-toggled');
+  if (previousButton) {
+    previousButton.classList.remove('is-toggled');
+  }
+} 
