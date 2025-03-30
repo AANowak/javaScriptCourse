ea0d74c96dbced2c9f5cc49fa10a2a6b84f06b65
A.A.N. <adrianna.a.nowak@wp.pl>
2025-03-30 04:50:20 +0200

    Arryas and Loops

--- 11 Arryas and Loops/11-todo-list-script.js
+++ 11 Arryas and Loops/11-todo-list-script.js
@@ -1,14 +1,23 @@
-const todoList = ['make dinner', 'wash dishes'];
+const todoList = [''];
 
-let todoListHTML = '';
+renderTodoList();
 
-for (let i = 0; i < todoList.length; i++) {
-  const todo = todoList[i];
-  const html = `<p>${todo}</p>`;
-  todoListHTML += html;
+function renderTodoList () {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
   }
 
   console.log(todoListHTML);
+
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
+}
+
+
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input')
@@ -18,5 +27,6 @@ function addTodo() {
   console.log(todoList);
 
   inputElement.value = '';
+  renderTodoList();
 }
 
