60191741cd9d80ab73c0d3a48f6b96b7c70055bf
AAN <adrianna.a.nowak@wp.pl>
2025-08-24 20:54:28 +0200

    calculator numpad event listener + minor styl changes

--- Projects/Calculator/calculator-script.js
+++ Projects/Calculator/calculator-script.js
@@ -11,3 +11,26 @@ function displayCalculation() {
   document.querySelector('.js-calculator-display')
     .innerHTML = calculation;
 }
+
+function resetCalculation() {
+  calculation = '';
+ displayCalculation();
+}
+
+function evaluateCalculation() {
+  eval(calculation);
+  calculation = eval(calculation);
+  displayCalculation();
+}
+
+
+document.addEventListener("keydown", (event) => {
+  const key = event.key;
+
+  if (/\d/.test(key)) addToCalculation(key);
+  else if ("+-*/".includes(key)) addToCalculation(` ${key} `);
+  else if (key === ".") addToCalculation(".");
+  else if (key === "Enter" || event.code === "NumpadEnter") evaluateCalculation();
+  else if (key === "(" || key === ")") addToCalculation(key);
+  else if (key === "Backspace") resetCalculation();
+});
